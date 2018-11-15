4949d8faa9b813f8f2d41eeb703d652fa5ae0646
Cloudflying <oss@live.hk>
2018-11-15 16:19:37 +0800

    modify catelog

--- images/cmdide/Dockerfile.php
+++ images/cmdide/Dockerfile.php
@@ -6,10 +6,14 @@ ENV USER=laravel \
 	PASSWD=laravel
 ENV HOME=/home/${USER}
 ADD script/laravel-alpine.sh /tmp/build.sh
+ADD script/alpine-runenv.sh /bin/runenv
 
 RUN chmod +x /tmp/build.sh \
-	&& sh /tmp/build.sh
+	&& chmod +x /bin/runenv \
+	&& sh /tmp/build.sh \
+	&& rm -fr /tmp/*
 
 USER ${USER}
 WORKDIR ${HOME}
+EXPOSE 80
 CMD ['/bin/sh']
